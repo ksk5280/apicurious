f772dd525617b1e34a2b924d21783afbe4f4198b
Kimiko Kano <kskano@gmail.com>
2016-04-07 14:41:08 -0600

    Add styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def followers
+    @followers = current_user.followers(current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'homes#show'
 
   get "/:username", to: "users#show", as: "user"
+  get "/:username/followers", to: "users#followers", as: "user_followers"
 
   get "/auth/github", as: :github_login
   get "/auth/github/callback", to: "sessions#create"
